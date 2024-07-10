9408b8dabce9f8eb1e63098c0cdfac08dd3fd1d9
Ifti81 <iftekharbd2002@yahoo.com>
2024-07-10 21:55:12 +0200

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,9 +10,9 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-// Filter the users array for any user with the same username and password
+// Check if the user with the given username and password exists
+const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
     let validusers = users.filter((user) => {
         return (user.username === username && user.password === password);
     });
@@ -24,12 +24,12 @@ const authenticatedUser = (username,password)=>{ //returns boolean
     }
 }
 
+
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
   const username = req.body.username;
     const password = req.body.password;
-
+    req.session.user = username;
     // Check if username or password is missing
     if (!username || !password) {
         return res.status(404).json({ message: "Error logging in" });
@@ -40,7 +40,7 @@ regd_users.post("/login", (req,res) => {
         // Generate JWT access token
         let accessToken = jwt.sign({
             data: password
-        }, 'access', { expiresIn: 60 * 60 });
+        }, 'access');
 
         // Store access token and username in session
         req.session.authorization = {
@@ -54,9 +54,32 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.session.user; // The username is stored in the req.session object
+
+  // Check if the book exists in the database
+  if (books.hasOwnProperty("ISBN")) {
+    const book = books["isbn"];
+
+    // Check if the user already has a review for the book
+    if (book.reviews.hasOwnProperty(username)) {
+      // Modify the existing review
+      book.reviews[username].review = review;
+      return res.status(200).json({ message: "Review modified successfully" });
+    } else {
+      // Add a new review for the user
+      book.reviews[username] = {
+        username: username,
+        review: review,
+      };
+      return res.status(200).json({ message: "Review added successfully" });
+    }
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
